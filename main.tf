1cd04791f83a798d33b42d7e9998c3b0558239f8
Diego Cristóbal Herreros <diecristher@gmail.com>
2021-11-27 14:50:52 +0100

    Feat: add master volume and pool

--- main.tf
+++ main.tf
@@ -12,3 +12,17 @@ required_version = ">= 0.13"
 provider "libvirt" {
   uri = "qemu://${var.server}/system"
 }
+
+
+resource "libvirt_pool" "pool" {
+  name = "my_pool"
+  type = "dir"
+  path = var.pool_path
+}
+
+resource "libvirt_volume" "k3s_master" {
+  name   = "k3s-master.raw"
+  size   = var.master_volume_size
+  format = "raw"
+  pool   = libvirt_pool.pool.name
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,14 @@ variable "server" {
     type        = string
     default     = "127.0.0.1"
 }
+
+variable "pool_path" {
+    description = "Path of the disks pool"
+    type        = string
+    default     = "/var/lib/libvirt/images"
+}
+
+variable "master_volume_size" {
+    description = "Volume size of master node"
+    type        = string
+}
